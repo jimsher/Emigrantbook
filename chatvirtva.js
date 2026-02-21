8cee1fc13d8ca38ae177289b1be5861fdecfa76d
jimsher <45567665+jimsher@users.noreply.github.com>
2026-02-21 11:13:20 +0100

    Create chatvirtva.js

---
+++ chatvirtva.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    const splash = document.getElementById('splash-screen');
+    
+    // 1.5 ან 2 წამი დააყოვნე, რომ იუზერმა ლოგო დაინახოს
+    setTimeout(() => {
+        splash.classList.add('fade-out');
+    }, 2000); 
+});
