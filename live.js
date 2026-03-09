360f5dd62b9e524500ca4c6a9788ea8b4d3cde9c
jimsher <45567665+jimsher@users.noreply.github.com>
2026-03-09 01:44:21 +0100

    Update live.js

--- live.js
+++ live.js
@@ -55,30 +55,6 @@ async function startLive() {
 
 
 
-// ეს ფუნქცია გამოიძახე როცა სტუმარს ათანხმებ (acceptGuest) ან როცა სტუმარი ერთვება
-function updateLiveLayout(isSplit) {
-    const hostWrap = document.getElementById('host-video-wrapper');
-    const guestBox = document.getElementById('guest-video-box');
-
-    if (isSplit) {
-        // ეკრანის გაყოფა 50/50-ზე
-        hostWrap.style.width = "50%";
-        guestBox.style.display = "block";
-        setTimeout(() => { guestBox.style.width = "50%"; }, 10);
-    } else {
-        // დაბრუნება სრულ ეკრანზე
-        hostWrap.style.width = "100%";
-        guestBox.style.width = "0%";
-        setTimeout(() => { guestBox.style.display = "none"; }, 400);
-    }
-}
-
-
-
-
-
-
-
 // --- ჩატის ლოგიკა (ავატარებით) ---
 function listenToLiveChat(channel) {
     const chatBox = document.getElementById('liveChatBox');
@@ -626,3 +602,20 @@ async function startGuestStreaming() {
 
 
 
+// ეს ფუნქცია გამოიძახე როცა სტუმარს ათანხმებ (acceptGuest) ან როცა სტუმარი ერთვება
+function updateLiveLayout(isSplit) {
+    const hostWrap = document.getElementById('host-video-wrapper');
+    const guestBox = document.getElementById('guest-video-box');
+
+    if (isSplit) {
+        // ეკრანის გაყოფა 50/50-ზე
+        hostWrap.style.width = "50%";
+        guestBox.style.display = "block";
+        setTimeout(() => { guestBox.style.width = "50%"; }, 10);
+    } else {
+        // დაბრუნება სრულ ეკრანზე
+        hostWrap.style.width = "100%";
+        guestBox.style.width = "0%";
+        setTimeout(() => { guestBox.style.display = "none"; }, 400);
+    }
+}
