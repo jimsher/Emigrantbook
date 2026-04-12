1ff612a56edeceb238d9da585a5501f33658da68
jimsher <45567665+jimsher@users.noreply.github.com>
2026-04-12 18:48:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2920,16 +2920,15 @@ function loadCommunityPosts() {
                 <div class="post-header" style="display:flex; justify-content:space-between; align-items:center; margin-bottom:10px;">
                     <div style="display:flex; align-items:center; gap:10px; cursor:pointer;" onclick="openProfile('${post.authorId}')">
                    <img src="${post.authorPhoto || 'https://ui-avatars.com/api/?name='+post.authorName}" style="width:35px; height:35px; border-radius:50%; border:1px solid var(--gold); object-fit:cover;">
+
+                   <div onclick="window.toggleWallTag('${id}')" style="cursor:pointer; display:flex; align-items:center; gap:6px;">
+                    <i class="${isTagged ? 'fas' : 'far'} fa-user-tag" style="${isTagged ? 'color:var(--gold);' : 'color:#888;'}"></i>
+                    <span style="font-size:14px; font-weight:bold;">${isTagged ? 'მონიშნულია' : 'მონიშვნა'}</span>
+                    </div>
     
                    <div style="display:flex; flex-direction:column; align-items:flex-start;">
                    <b style="color:white; font-size:14px; margin:0; line-height:1.2;">${post.authorName}</b>
                     <span style="color:#888; font-size:10px; margin-top:2px; display:block;">${postTime}</span>
-
-
-                    <div onclick="window.toggleWallTag('${id}')" style="cursor:pointer; display:flex; align-items:center; gap:6px;">
-                    <i class="${isTagged ? 'fas' : 'far'} fa-user-tag" style="${isTagged ? 'color:var(--gold);' : 'color:#888;'}"></i>
-                    <span style="font-size:14px; font-weight:bold;">${isTagged ? 'მონიშნულია' : 'მონიშვნა'}</span>
-                    </div>
                     </div>
                     </div>
                     
