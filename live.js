d054a27fc88b3749125e7aef1a0b4030feb78e84
jimsher <45567665+jimsher@users.noreply.github.com>
2026-04-26 09:06:13 +0200

    Update live.js

--- live.js
+++ live.js
@@ -6,7 +6,7 @@ function startLiveFunc() { toggleSideMenu(false); startLive(); }
 
 async function startLive() {
     const appId = "7290502fac7f4feb82b021ccde79988a"; 
-    const token = "007eJxTYLB6xlC05H7LAncx9mOfb0e4ZR370LxH7MZ6YwGd6+qMVqUKDOZGlgamBkZpicnmaSZpqUkWRkkGRobJySmp5paWFhaJ31PfZDYEMjKcnjqVmZEBAkF8boaczLLU+OKSotTEXAYGAD0AIxk=";
+    const token = "007eJxTYIhZxZ5rKBZWqdrvNnVS3gVOIf/Ognlz2HTuHi5pk/rZIqfAYG5kaWBqYJSWmGyeZpKWmmRhlGRgZJicnJJqbmlpYZF4fcfbzIZARoY+2WQmRgYIBPG5GXIyy1Lji0uKUhNzGRgAZF0gig==";
     
     currentLiveChannel = "live_stream"; 
     
@@ -88,7 +88,7 @@ function loadActiveLives() {
 
 async function joinLive(hostUid, channelName) {
     const appId = "7290502fac7f4feb82b021ccde79988a"; 
-    const token = "007eJxTYLB6xlC05H7LAncx9mOfb0e4ZR370LxH7MZ6YwGd6+qMVqUKDOZGlgamBkZpicnmaSZpqUkWRkkGRobJySmp5paWFhaJ31PfZDYEMjKcnjqVmZEBAkF8boaczLLU+OKSotTEXAYGAD0AIxk=";
+    const token = "007eJxTYIhZxZ5rKBZWqdrvNnVS3gVOIf/Ognlz2HTuHi5pk/rZIqfAYG5kaWBqYJSWmGyeZpKWmmRhlGRgZJicnJJqbmlpYZF4fcfbzIZARoY+2WQmRgYIBPG5GXIyy1Lji0uKUhNzGRgAZF0gig==";
     currentLiveChannel = channelName;
     
     document.getElementById('liveUI').style.display = 'flex';
