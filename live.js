3da28b05f77e2b4415bc0dea9cf8aee4c4788d38
jimsher <45567665+jimsher@users.noreply.github.com>
2026-04-29 00:08:33 +0200

    Update live.js

--- live.js
+++ live.js
@@ -23,7 +23,7 @@ const liveGiftsLibrary = [
         id: "rose", 
         name: "Rose", 
         price: 1, 
-        img: "https://cdn.jsdelivr.net/gh/jimsher/Emigrantbook@main/Vefxvi.gif",
+        img: "https://cdn.jsdelivr.net/gh/jimsher/Emigrantbook@main/Vefxvi.1.gif",
         sound: "https://github.com/jimsher/Emigrantbook/blob/main/u_edtmwfwu7c-pop-331049.mp3" 
     }
 ];
