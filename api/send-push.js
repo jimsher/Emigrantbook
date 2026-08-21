76c9968f7c4b8ff0cd5616631bc6f68d3cc9b572
jimsher <45567665+jimsher@users.noreply.github.com>
2026-08-21 21:40:52 +0200

    Update send-push.js

--- api/send-push.js
+++ api/send-push.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // CORS-ის ნებართვა
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -12,14 +11,22 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
+  const ONE_SIGNAL_APP_ID = "5b8f7b19-f368-418a-b87b-f7582d331fae";
+  const REST_API_KEY = "os_v2_app_lohxwgptnbayvod365mc2my7vzv4mz2abm4enhmpskzsyeqtwbx3a4n33clccezkigbl4hbkcsymizeka3o3lnnmbtwxqe4o3huynzy";
+
   try {
+    const pushPayload = {
+      app_id: ONE_SIGNAL_APP_ID,
+      ...req.body
+    };
+
     const response = await fetch("https://onesignal.com/api/v1/notifications", {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": "Key os_v2_app_lohxwgptnbayvod365mc2my7vzv4mz2abm4enhmpskzsyeqtwbx3a4n33clccezkigbl4hbkcsymizeka3o3lnnmbtwxqe4o3huynzy"
+        "Authorization": `Key ${REST_API_KEY}`
       },
-      body: JSON.stringify(req.body)
+      body: JSON.stringify(pushPayload)
     });
 
     const data = await response.json();
