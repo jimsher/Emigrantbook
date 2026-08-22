9cb24070a72a630379ec525cf3eab739ad49e683
jimsher <45567665+jimsher@users.noreply.github.com>
2026-08-22 09:50:35 +0200

    Update send-push.js

--- api/send-push.js
+++ api/send-push.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   const ONE_SIGNAL_APP_ID = "5b8f7b19-f368-418a-b87b-f7582d331fae";
-  const ORG_API_KEY = "os_v2_org_ohkifot4rjcovl4ogegabgduycyjl5cl4fiuzlnrzjqn2aw6kg63gxikmsdnsengoqamgua7kka6gy2svbs2wz4m32g4pfwvcwiqlci";
+  const ORG_API_KEY = "os_v2_org_ohkifot4rjcovl4ogegabgduycyjl5cl4fiuzlnrzjqn2aw6kg63gxikmsdnsengoqamgua7kka6gy2svbs2wz4n32g4pfwvcwiqlci";
 
   try {
     const bodyData = typeof req.body === 'string' ? JSON.parse(req.body) : (req.body || {});
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.onesignal.com/notifications", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
         "accept": "application/json",
         "Authorization": `Key ${ORG_API_KEY.trim()}`
       },
